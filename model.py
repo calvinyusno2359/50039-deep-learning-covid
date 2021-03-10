0bd98cc87f9274c38748987a5c1c1312844b51ce
heavensward.argentlux <heavensward.argentlux@gmail.com>
2021-03-10 13:22:32 +0800

    working copy dataloader and model

--- model.py
+++ model.py
@@ -1,3 +1,18 @@
+from dataloader import DataLoader, Lung_Train_Dataset
+# Matplotlib
+import matplotlib.pyplot as plt
+# Numpy
+import numpy as np
+# Pillow
+from PIL import Image
+# Torch
+import torch
+from torch.utils.data import Dataset, DataLoader
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torchvision import transforms
+
 # A simple mode
 class Net(nn.Module):
     def __init__(self):
@@ -15,6 +30,10 @@ class Net(nn.Module):
 
 # Create model
 model = Net()
+
+bs_val = 4
+ld_train = Lung_Train_Dataset()
+train_loader = DataLoader(ld_train, batch_size = bs_val, shuffle = True)
 
 # Try model on one mini-batch
 for batch_idx, (images_data, target_labels) in enumerate(train_loader):
