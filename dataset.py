cdc0810724739be24d6a734e4a3e05855602ee18
heavensward.argentlux <heavensward.argentlux@gmail.com>
2021-03-10 14:06:00 +0800

    clean dataset

--- dataset.py
+++ dataset.py
@@ -1,5 +1,76 @@
+import numpy as np
+from PIL import Image
+from torch.utils.data import Dataset, DataLoader
+
+class Image_Dataset(Dataset):
+	def __init__(self, img_size, class_dict, groups, dataset_numbers, dataset_paths):
+		self.img_size = img_size
+		self.classes = class_dict
+		self.groups = groups
+		self.dataset_numbers = dataset_numbers
+		self.dataset_paths = dataset_paths
+
+	def describe(self):
+		msg = f"""
+		It contains a total of {sum(self.dataset_numbers.values())} images of size {self.img_size}.
+		Images have been split in {len(self.groups)} groups: {self.groups} sets.
+		The images are stored in the following locations, each containing the following images:
+		"""
+		print(msg)
+
+		for key, val in self.dataset_paths.items():
+			print(f" - {key}, in folder {val}: {self.dataset_numbers[key]} images.")
+
+	def open_img(self, group_val, class_val, index_val):
+		err_msg = f"Error - group_val variable should be set to {self.groups}."
+		assert group_val in self.groups, err_msg
+
+		err_msg = f"Error - class_val variable should be set to {self.classes.values()}."
+		assert class_val in self.classes.values(), err_msg
+
+		max_val = self.dataset_numbers[f'{group_val}_{class_val}']
+		err_msg = "Error - index_val variable should be an integer between 0 and the maximal number of images."
+		err_msg += f"\n(In {group_val}/{class_val}, you have {max_val} images.)"
+		assert isinstance(index_val, int), err_msg
+		assert index_val >= 0 and index_val <= max_val, err_msg
+
+		dataset_path = self.dataset_paths[f'{group_val}_{class_val}']
+		path_to_file = f'{dataset_path}/{index_val}.jpg'
+
+		with open(path_to_file, 'rb') as f:
+			im = np.asarray(Image.open(f)) / 255
+		f.close()
+		return im
+
+	def show_img(self, group_val, class_val, index_val):
+		im = self.open_img(group_val, class_val, index_val)
+		plt.imshow(im)
 
 if __name__ == "__main__":
 	dataset_path = r'./dataset_demo'
 
-	print(dataset_path)
+	img_size = (150, 150)
+	class_dict = {0: 'normal', 1: 'infected'}
+	groups = ['train', 'test', 'val']
+	dataset_numbers = { 'train_normal': 36,
+											'train_infected': 34,
+											'val_normal': 4,
+											'val_infected': 4,
+											'test_normal': 14,
+											'test_infected': 13
+										}
+
+	dataset_paths = { 'train_normal': './dataset_demo/train/normal/',
+										'train_infected': './dataset_demo/train/infected/',
+										'val_normal': './dataset_demo/val/normal/',
+										'val_infected': './dataset_demo/val/infected/',
+										'test_normal': './dataset_demo/test/normal/',
+										'test_infected': './dataset_demo/test/infected/'
+									}
+
+	lung_dataset = Image_Dataset(img_size, class_dict, groups, dataset_numbers, dataset_paths)
+	print(lung_dataset.describe())
+
+	im = lung_dataset.open_img('train', 'normal', 1)
+	print(im.shape)
+	print(im)
