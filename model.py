453ffee129fb35362a2f57661a4e317c906e9dce
jesterlyn <worldofcaramelaw@gmail.com>
2021-03-17 12:12:19 +0800

    added parameters to net()

--- model.py
+++ model.py
@@ -7,7 +7,7 @@ from torch.utils.data import DataLoader
 
 
 class Net(nn.Module):
-    def __init__(self):
+    def __init__(self, numberOfOutputLabels):
         super().__init__()
         self.layers = nn.Sequential(
             nn.Conv2d(in_channels=1, out_channels=32, kernel_size=7, padding=3, bias=False),
@@ -53,14 +53,16 @@ class Net(nn.Module):
                 )
             ),
             nn.AdaptiveAvgPool2d(1),
-            nn.Flatten(),
-            nn.Linear(32, 3),
+            nn.Flatten()
         )
+        self.fc = nn.Linear(32, numberOfOutputLabels)
 
     def forward(self, x):
         x = self.layers(x)
+        x = self.fc(x)
         output = F.log_softmax(x, dim=1)
         return output
+
 
 class ResBlock(nn.Module):
     def __init__(self, module):
@@ -69,6 +71,7 @@ class ResBlock(nn.Module):
 
     def forward(self, inputs):
         return self.module(inputs) + inputs
+
 
 class DenseNet(nn.Module):
     def __init__(self, nr_classes):
@@ -118,6 +121,7 @@ class DenseNet(nn.Module):
         out = self.classifier(out)
         return out
 
+
 class DenseBlock(nn.Module):
     def __init__(self, in_channels):
         super(DenseBlock, self).__init__()
@@ -160,30 +164,33 @@ class TransitionLayer(nn.Module):
         out = self.avg_pool(bn)
         return out
 
+
 if __name__ == "__main__":
-	# Create model
-	model = Net()
+    # Create model
 
-	img_size = (150, 150)
-	class_dict = {0: 'normal', 1: 'infected'}
-	groups = ['train']
-	dataset_numbers = {'train_normal': 36,
-	                   'train_infected': 34,
-	                   }
+    # for 2 class
+    model = Net(numberOfOutputLabels=2)
 
-	dataset_paths = {'train_normal': './dataset_demo/train/normal/',
-	                 'train_infected': './dataset_demo/train/infected/',
-	                 }
+    img_size = (150, 150)
+    class_dict = {0: 'normal', 1: 'infected'}
+    groups = ['train']
+    dataset_numbers = {'train_normal': 36,
+                       'train_infected': 34,
+                       }
 
-	bs_val = 4
-	ld_train = BinaryClassDataset('train', img_size, class_dict, groups, dataset_numbers, dataset_paths)
-	train_loader = DataLoader(ld_train, batch_size=bs_val, shuffle=True)
+    dataset_paths = {'train_normal': './dataset_demo/train/normal/',
+                     'train_infected': './dataset_demo/train/infected/',
+                     }
 
-	# Try model on one mini-batch
-	for batch_idx, (images_data, target_labels) in enumerate(train_loader):
-		predicted_labels = model(images_data)
-		print(predicted_labels)
-		print(target_labels)
-		# Forced stop
-		break
-		# assert False, "Forced stop after one iteration of the mini-batch for loop"
+    bs_val = 4
+    ld_train = BinaryClassDataset('train', img_size, class_dict, groups, dataset_numbers, dataset_paths)
+    train_loader = DataLoader(ld_train, batch_size=bs_val, shuffle=True)
+
+    # Try model on one mini-batch
+    for batch_idx, (images_data, target_labels) in enumerate(train_loader):
+        predicted_labels = model(images_data)
+        print(predicted_labels)
+        print(target_labels)
+        # Forced stop
+        break
+        # assert False, "Forced stop after one iteration of the mini-batch for loop"
