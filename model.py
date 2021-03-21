062ed4669c134cfac01e54524f2b2cc2cce8eae9
heavensward.argentlux <heavensward.argentlux@gmail.com>
2021-03-21 12:24:33 +0800

    cleanup report

--- model.py
+++ model.py
@@ -68,57 +68,57 @@ class ResBlock(nn.Module):
 			    nn.BatchNorm2d(out_channels),
 			)
 
-    def forward(self, x):
-        res = x
-        # print(res)
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.conv2(x)
-        x = self.bn2(x)
-        if self.downsample is not None:
-            res = self.downsample(res)
-        # print(res.shape)
-        x += res
-        x = self.relu(x)
-        return x
+	def forward(self, x):
+		res = x
+		# print(res)
+		x = self.conv1(x)
+		x = self.bn1(x)
+		x = self.relu(x)
+		x = self.conv2(x)
+		x = self.bn2(x)
+		if self.downsample is not None:
+		    res = self.downsample(res)
+		# print(res.shape)
+		x += res
+		x = self.relu(x)
+		return x
 
 # ___________________________________________ Helper Functions ___________________________________________ #
 class PrintLayer(nn.Module):
-    def __init__(self):
-        super(PrintLayer, self).__init__()
+	def __init__(self):
+		super(PrintLayer, self).__init__()
 
-    def forward(self, x):
-        print(x.shape)
-        return x
+	def forward(self, x):
+		print(x.shape)
+		return x
 
 
 if __name__ == "__main__":
-    # Create model
+	# Create model
 
-    # for 2 class
-    model = Net(2)
+	# for 2 class
+	model = ResNet(2)
 
-    img_size = (150, 150)
-    class_dict = {0: 'normal', 1: 'infected'}
-    groups = ['train']
-    dataset_numbers = {'train_normal': 36,
-                       'train_infected': 34,
-                       }
+	img_size = (150, 150)
+	class_dict = {0: 'normal', 1: 'infected'}
+	groups = ['train']
+	dataset_numbers = {'train_normal': 36,
+	                   'train_infected': 34,
+	                   }
 
-    dataset_paths = {'train_normal': './dataset_demo/train/normal/',
-                     'train_infected': './dataset_demo/train/infected/',
-                     }
+	dataset_paths = {'train_normal': './dataset_demo/train/normal/',
+	                 'train_infected': './dataset_demo/train/infected/',
+	                 }
 
-    bs_val = 4
-    ld_train = TrinaryClassDataset('train', img_size, class_dict, groups, dataset_numbers, dataset_paths)
-    train_loader = DataLoader(ld_train, batch_size=bs_val, shuffle=True)
+	bs_val = 4
+	ld_train = BinaryClassDataset('train', img_size, class_dict, groups, dataset_numbers, dataset_paths)
+	train_loader = DataLoader(ld_train, batch_size=bs_val, shuffle=True)
 
-    # Try model on one mini-batch
-    for batch_idx, (images_data, target_labels) in enumerate(train_loader):
-        predicted_labels = model(images_data)
-        print(predicted_labels)
-        print(target_labels)
-        # Forced stop
-        break
-        # assert False, "Forced stop after one iteration of the mini-batch for loop"
+	# Try model on one mini-batch
+	for batch_idx, (images_data, target_labels) in enumerate(train_loader):
+		predicted_labels = model(images_data)
+		print(predicted_labels)
+		print(target_labels)
+		# Forced stop
+		break
+		# assert False, "Forced stop after one iteration of the mini-batch for loop"
